1d0e19cf9d76180eb97c180d7105bcd327b9b318
hinesnoa@msu.edu <hinesnoa@msu.edu>
2015-06-16 22:53:48 -0500

    Automated commit at 2015-06-17 03:53:48 UTC by middleman-deploy 1.0.0

---
+++ javascripts/app.js
@@ -0,0 +1,73 @@
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 400);
+        return false;
+      }
+    }
+  });
+
+
+  var data = [
+  { 
+    action: 'type',
+    strings: ["npm install -g Noah^400"],
+    output: '<span class="gray">+Noah@0.21.0 installed</span><br>&nbsp;',
+    postDelay: 1000
+  },
+  { 
+    action: 'type',
+    //clear: true,
+    strings: ['Noah^400'],
+    output: $('.noah-run-output').html()
+  }  
+];
+  runScripts(data, 0);
+});
+
+function runScripts(data, pos) {
+    var prompt = $('.prompt'),
+        script = data[pos];
+    if(script.clear === true) {
+      $('.history').html(''); 
+    }
+    switch(script.action) {
+        case 'type':
+          // cleanup for next execution
+          prompt.removeData();
+          $('.typed-cursor').text('');
+          prompt.typed({
+            strings: script.strings,
+            typeSpeed: 3,
+            callback: function() {
+              var history = $('.history').html();
+              history = history ? [history] : [];
+              history.push('$ ' + prompt.text());
+              if(script.output) {
+                history.push(script.output);
+                prompt.html('');
+                $('.history').html(history.join('<br>'));
+              }
+              // scroll to bottom of screen
+              $('section.terminal').scrollTop($('section.terminal').height());
+              // Run next script
+              pos++;
+              if(pos < data.length) {
+                setTimeout(function() {
+                  runScripts(data, pos);
+                }, script.postDelay || 1000);
+              }
+            }
+          });
+          break;
+        case 'view':
+
+          break;
+    }
+}
+;
